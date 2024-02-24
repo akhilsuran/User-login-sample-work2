a0ef42397ff1443df1afd317a088a38fb9a3989e
Akhilzenys <75058471+Akhilzenys@users.noreply.github.com>
2024-02-24 16:32:48 +0530

    login form done

---
+++ User Registration from/src/components/UserProfile.vue
@@ -0,0 +1,50 @@
+<!-- <template>
+    <div>
+        <h2>Welcome, {{ username }}!</h2>
+        <p>Email: {{ email }}</p>
+    </div>
+</template> -->
+<template>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card text-center">
+            <div class="card-header">Welcome, {{ username }}!</div>
+            <img class="card-img-top mx-auto d-block" src="@/assets/profile.png" alt="Profile Photo" style="width: 40%; height: auto;">
+            <div class="card-body">
+              <div class="form-group">
+                <label>Name:</label>
+                <p class="form-control-static">{{ username }}</p>
+              </div>
+              <div class="form-group">
+                <label>Email:</label>
+                <p class="form-control-static">{{ email }}</p>
+              </div>
+              <router-link :to="'/register/return'" class="btn btn-primary">Logout</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const username = ref('');
+const email = ref('');
+onMounted(() => {
+
+    const savedUsername = localStorage.getItem('username');
+    const savedEmail = localStorage.getItem('email');
+    username.value = savedUsername;
+    email.value= savedEmail;
+});
+
+// export default {
+//     props: {
+//         username: String,
+//         email: String
+//     },
+//   };
+</script>
---
+++ User Registration from/src/components/registrationForm.vue
@@ -0,0 +1,61 @@
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header" style="text-align: center;">LOGIN</div>
+                    <div class="card-body">
+                        <form @submit.prevent="registerUser">
+                            <div class="form-group">
+                                <label for="email">Username:</label>
+                                <input class="form-control" type="text" id="username" v-model="username" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email address:</label>
+                                <input type="email" class="form-control" id="email" v-model="email" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input type="password" class="form-control" id="password" v-model="password" required>
+                            </div>
+                            <br>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script >
+
+export default {
+    data() {
+        return {
+            id: this.$route.params.id,
+            username: '',
+            email: '',
+            password: ''
+        };
+    },
+    created() {
+        if (this.id) {
+            localStorage.clear();
+            console.log("storage cleared")
+        }
+    },
+    methods: {
+        registerUser() {
+            localStorage.setItem('username', this.username);
+            localStorage.setItem('email', this.email);
+            this.$router.push('/profile', {
+                username: this.username,
+                email: this.email
+            });
+        }
+    }
+};
+</script>
+    
